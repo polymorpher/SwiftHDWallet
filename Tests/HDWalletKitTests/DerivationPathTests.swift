db37ee572c9280dcff582efad28e5e8f9a42a7a9
Ronald Mannak <ronald@starlingprotocol.com>
2021-08-05 08:49:12 -0700

    Add test for Ledger derivation path standard

--- Tests/HDWalletKitTests/DerivationPathTests.swift
+++ Tests/HDWalletKitTests/DerivationPathTests.swift
@@ -9,10 +9,16 @@ import XCTest
 @testable import HDWalletKit
 
 class DerivationPathTests: XCTestCase {
+    
+    private var mnemonic: String!
+    
+    override func setUp() {
+        let entropy = Data(hex: "000102030405060708090a0b0c0d0e0f")
+        mnemonic = Mnemonic.create(entropy: entropy)
+    }
+    
     func testDerivationPath() {
         
-        let entropy = Data(hex: "000102030405060708090a0b0c0d0e0f")
-        let mnemonic = Mnemonic.create(entropy: entropy)
         XCTAssertEqual(mnemonic, "abandon amount liar amount expire adjust cage candy arch gather drum buyer")
         let seed = Mnemonic.createSeed(mnemonic: mnemonic)
         
@@ -34,7 +40,46 @@ class DerivationPathTests: XCTestCase {
         
         XCTAssertEqual(change.get(), derivatedPrivateKey.get())
         XCTAssertEqual(change.publicKey.address, derivatedPrivateKey.publicKey.address)
+        
         XCTAssertNotEqual(privateKey.get(), derivatedPrivateKey.get())
         XCTAssertNotEqual(privateKey.publicKey.address, derivatedPrivateKey.publicKey.address)
+        
+        XCTAssertEqual(privateKey.publicKey.address, "0xb538E2A54a8F3021cB064dDD18dc3700D6215612")
+        XCTAssertEqual(change.publicKey.address, "0x6cd36F07a758152A04044d488AcD90F6798f245d")
+    }
+    
+    func testDerivationPathLedger() {
+
+        XCTAssertEqual(mnemonic, "abandon amount liar amount expire adjust cage candy arch gather drum buyer")
+        let seed = Mnemonic.createSeed(mnemonic: mnemonic)
+        
+        let privateKey = PrivateKey(seed: seed, coin: .ethereum)
+        // BIP44 key derivation
+        // m/44'
+        let purpose = privateKey.derived(at: .hardened(44))
+
+        // m/44'/60'
+        let coinType = purpose.derived(at: .hardened(60))
+
+        // m/44'/60'/12'
+        let account = coinType.derived(at: .hardened(12))
+
+        // m/44'/60'/0'/0
+        let change = account.derived(at: .notHardened(0))
+        
+        // m/44'/0'/0'/0/0
+        let addressIndex = change.derived(at: .notHardened(0))
+
+        let derivatedPrivateKey = try! privateKey.privateKey(at: "m/44'/60'/12'/0/0")
+
+        XCTAssertEqual(addressIndex.get(), derivatedPrivateKey.get())
+        XCTAssertEqual(addressIndex.publicKey.address, derivatedPrivateKey.publicKey.address)
+        
+        XCTAssertNotEqual(privateKey.get(), derivatedPrivateKey.get())
+        XCTAssertNotEqual(privateKey.publicKey.address, derivatedPrivateKey.publicKey.address)
+        
+        XCTAssertEqual(privateKey.publicKey.address, "0xb538E2A54a8F3021cB064dDD18dc3700D6215612")
+        XCTAssertEqual(addressIndex.publicKey.address, "0xe4967065874AFE4cb578F423c4B489D4ECf3BB2f")
+        
     }
 }
